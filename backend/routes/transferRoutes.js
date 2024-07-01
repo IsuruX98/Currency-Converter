68ae4c8293a94b0eb75e35e447f72b15deb19dc0
IsuruX98 <isurusanka98@gmail.com>
2024-07-01 09:58:30 +0530

    route protection added

--- backend/routes/transferRoutes.js
+++ backend/routes/transferRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const transferController = require("../controllers/transferController");
-const protect = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/rate", transferController.getRate);
